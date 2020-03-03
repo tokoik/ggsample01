9a0d5b9ec31796d62946cfdbfa11d2747c4ec163
Kohe Tokoi <tokoik@gmail.com>
2020-03-03 14:31:51 +0900

    upgrade to Dear ImGui 1.75

--- include/imstb_rectpack.h
+++ include/imstb_rectpack.h
@@ -1,4 +1,4 @@
-// [DEAR IMGUI]
+﻿// [DEAR IMGUI]
 // This is a slightly modified version of stb_rect_pack.h 1.00.
 // Those changes would need to be pushed into nothings/stb:
 // - Added STBRP__CDECL
